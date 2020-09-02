4065975f88732a849ce4d46f8993af2578c537cf
Digital (Cipher) Lucifer <39482961+dls-cipher@users.noreply.github.com>
2020-09-03 01:47:10 +0700

    Create full_install.sh

---
+++ full_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+chmod a+x .sh
+./building-env.sh
+
+echo ""
+read -p "Step 1/3 - Building environment is done." -t 3
+clear
+echo ""
+./nginx_ssl.sh
+echo ""
+read -p "Step 2/3 - NGINX, WebSocket and SSL Installation is done." -t 3
+echo ""
+clear
+./api_deploy.sh 
+echo ""
+read -p "Step 3/3/ - Meta1API is up and running." -t 5
+clear
+echo "Process successfully done." 
